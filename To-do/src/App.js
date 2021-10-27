6ec4a54137c86286b5747fa48f16da8c68884970
supersay245 <samoa4208@protonmail.com>
2021-10-27 14:20:22 -0500

    styling

--- To-do/src/App.js
+++ To-do/src/App.js
@@ -4,6 +4,7 @@ import Chore from './components/Chore';
 import Nav from './components/Nav.js'
 import { Route } from "react-router-dom";
 import 'react-calendar/dist/Calendar.css'; 
+import AboutUs from './components/About Us';
 
 
 
--- To-do/src/components/Nav.js
+++ To-do/src/components/Nav.js
@@ -4,8 +4,8 @@ function Nav(props) {
   return (
     <nav>
       <Link to="/" className="headers">Home</Link>
-      {/* <Link to="/About Us" className="headers">About Us</Link>
-          <Link to='/Recommendations' className="headers">Recommendations</Link> */}
+      <Link to="/About Us" className="headers">About Us</Link>
+      {/* <Link to='/Recommendations' className="headers">Recommendations</Link>  */}
       <Link to='/List/Chores/' className="headers">Chores</Link>
       {/* <Link to='/Errands' className="headers">Errands</Link>
       <Link to='/Fun' className="headers">Fun</Link> */}
